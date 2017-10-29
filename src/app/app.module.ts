48e49eb54d6d3bf42bc47f3bba63b6cab8ae358d
giuseppe Varas <giuseppe.varas@efby.cl>
2017-10-29 02:03:41 -0300

    creacion navbar, imagenes y compo0nentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent  } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
     
   ],
   imports: [
